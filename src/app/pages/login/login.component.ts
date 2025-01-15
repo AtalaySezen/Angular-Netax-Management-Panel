e6d09683458c0044ec5533a9754b3cf7d7fcf45e
AtalaySezen <88587309+AtalaySezen@users.noreply.github.com>
2025-01-15 17:10:41 +0300

    feat: login sayfası tamamlandı, auth.service api istekleri eklendi.

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,11 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { AuthService } from '../../shared/services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [MatFormFieldModule, FormsModule, CommonModule, ReactiveFormsModule, MatInputModule, MatIconModule, MatButtonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  authService = inject(AuthService);
+  router = inject(Router);
+
+  loginForm = new FormGroup({
+    username: new FormControl('', [Validators.required, Validators.minLength(4)]),
+    password: new FormControl('', [Validators.required, Validators.minLength(3)]),
+  });
+
+  login() {
+    if (this.loginForm.valid) {
+      this.authService.Login(this.loginForm.value.username!, this.loginForm.value.password!).subscribe({
+        next: data => {
+          const accessToken = data.accessToken;
+          const userId = data.id;
+          this.authService.SetLocalStorageToken({
+            accessToken: accessToken,
+            id: userId
+          });
+          this.router.navigate(['home']);
+        }, error: (err) => {
+          console.error(err.error.message)
+        }
+      })
+    }
+
+  }
+
+
 
 }
--- src/app/shared/services/auth.service.ts
+++ src/app/shared/services/auth.service.ts
@@ -1,9 +1,35 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { environment } from '../../../environments/environment.development';
+import { Observable } from 'rxjs';
+import { Auth, AuthData, user } from '../models/auth.model';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  http = inject(HttpClient);
+  router = inject(Router);
+  authLocalStorageToken = `${environment.appVersion}-${environment.APP_KEY}`;
 
-  constructor() { }
+  Login(username: string, password: string): Observable<Auth<AuthData>> {
+    return this.http.post<Auth<AuthData>>(environment.apiUrl + 'auth/login',
+      {
+        username: username,
+        password: password
+      }
+    )
+  }
+
+  SetLocalStorageToken(user: user) {
+    localStorage.setItem(`${this.authLocalStorageToken}-token`, user.accessToken);
+    localStorage.setItem(`${this.authLocalStorageToken}-userId`, user.id.toString());
+  }
+
+  LogOut() {
+    localStorage.removeItem(`${this.authLocalStorageToken}`);
+    this.router.navigate(['/login']);
+  }
+
 }
