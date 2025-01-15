e66f52357fb8b87337f03a6bb8a635db0b8b2e72
AtalaySezen <88587309+AtalaySezen@users.noreply.github.com>
2025-01-15 16:43:07 +0300

    feats : app.routes.ts düzenlendi.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { HomeComponent } from './pages/home/home.component';
+import { authGuard } from './shared/auth/auth.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', pathMatch: 'full', redirectTo: 'home' },
+    { path: 'login', component: LoginComponent, title: 'Giriş' },
+    { path: 'home', canActivate: [authGuard], component: HomeComponent, title: 'Anasayfa' }
+];
