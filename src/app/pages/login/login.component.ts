5d520c4fb0225ac99f86aab71ffa9ef4d72a1582
AtalaySezen <88587309+AtalaySezen@users.noreply.github.com>
2025-01-16 23:37:08 +0300

    feat : giriş sayfasına snackbar eklendi.

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
@@ -7,6 +7,7 @@ import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } 
 import { CommonModule } from '@angular/common';
 import { AuthService } from '../../shared/services/auth.service';
 import { Router } from '@angular/router';
+import { MatSnackBar, MatSnackBarHorizontalPosition, MatSnackBarVerticalPosition } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-login',
@@ -17,23 +18,37 @@ import { Router } from '@angular/router';
 export class LoginComponent {
   authService = inject(AuthService);
   router = inject(Router);
+  snackBar = inject(MatSnackBar);
+  horizontalPosition: MatSnackBarHorizontalPosition = 'right';
+  verticalPosition: MatSnackBarVerticalPosition = 'top';
 
   loginForm = new FormGroup({
     username: new FormControl('', [Validators.required, Validators.minLength(4)]),
     password: new FormControl('', [Validators.required, Validators.minLength(3)]),
   });
 
+
   login() {
     if (this.loginForm.valid) {
       this.authService.Login(this.loginForm.value.username!, this.loginForm.value.password!).subscribe({
         next: data => {
+          this.openSnackBar('Başarılı', 'Ok');
           this.authService.SetLocalStorageToken(data.accessToken);
           this.router.navigate(['home']);
         }, error: (err) => {
           console.error(err.error.message)
+          this.openSnackBar(err.error.message, 'Ok');
         }
       })
     }
+  }
+
+
+  openSnackBar(message: string, action: string) {
+    this.snackBar.open(message, action, {
+      horizontalPosition: this.horizontalPosition,
+      verticalPosition: this.verticalPosition,
+    });
   }
 
 }
